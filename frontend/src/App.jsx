d026de33e4e32873239fa3cef7c06791c09e231c
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-12 13:28:48 -0500

    Updated App.jsx to support routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BackgroundBox from "./components/layouts/Background";
 import "./styles/App.css";
 import Navbar from "./components/navbar/Navbar";
-// import Realtime from "./pages/Realtime";
-// import WSN from "./pages/WSN";
+import Realtime from "./pages/Realtime";
+import WSN from "./pages/WSN";
 import Download from "./pages/Download";
 
 const App = () => {
   return (
-    <div className="app-background">
-      <BackgroundBox>
-        <Navbar />
-
-        {/* <Realtime /> */}
-        {/* <WSN /> */}
-        <Download />
-      </BackgroundBox>
-    </div>
+    <Router>
+      <div className="app-background">
+        <BackgroundBox>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Realtime />} />
+            <Route path="/wsn" element={<WSN />} />
+            <Route path="/download" element={<Download />} />
+          </Routes>
+        </BackgroundBox>
+      </div>
+    </Router>
   );
 };
 
