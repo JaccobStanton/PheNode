d3da38bf67c115281192a2ed57346913efcf3a9b
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-20 17:47:39 -0500

    Made imaging icon responsive to being hovered over

--- frontend/src/components/pages/download/Title.jsx
+++ frontend/src/components/pages/download/Title.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import DataPreferenceSvg from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon.svg";
+import React, { useState } from "react";
+import DataPreferenceInactive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Inactive.svg";
+import DataPreferenceActive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Active.svg";
 
 function DownloadTitle() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <>
       <div className="grid-item page-title">Download Data</div>
       <div className="grid-item cell-wifi-last-measurements">
         <div className="download-data-preferences-box">
           <span className="preferences-text">To Data Download Preferences</span>
-          <div className="data-preference-svg-container">
+          <div
+            className="data-preference-svg-container"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
             <img
-              src={DataPreferenceSvg}
+              src={isHovered ? DataPreferenceActive : DataPreferenceInactive}
               alt="Data Preference Icon"
               className="data-preference-svg"
             />
