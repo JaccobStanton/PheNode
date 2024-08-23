60ce6e5ede35d977c59c27ec04b4e0dae2b0a1eb
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-23 16:20:17 -0500

    Added functionality to navigate to another component

--- frontend/src/components/pages/download/Title.jsx
+++ frontend/src/components/pages/download/Title.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import DataPreferenceInactive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Inactive.svg";
 import DataPreferenceActive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Active.svg";
 
 function DownloadTitle() {
   const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
 
+  const handleNavigate = () => {
+    navigate("/preferences");
+  };
   return (
     <>
       <div className="grid-item page-title">Download Data</div>
       <div className="grid-item cell-wifi-last-measurements">
-        <div className="download-data-preferences-box">
+        <div className="download-data-preferences-box" onClick={handleNavigate}>
           <span className="preferences-text">To Data Download Preferences</span>
           <div
             className="data-preference-svg-container"
