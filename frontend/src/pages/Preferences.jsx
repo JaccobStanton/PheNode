d513a33c9a61416d3d36705f7dae02c98265de13
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-23 16:18:00 -0500

    Finished download preferences component for the 2560px breakpoint

--- frontend/src/pages/Preferences.jsx
+++ frontend/src/pages/Preferences.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PreferencesTitle from "../components/pages/preferences/Title";
+import DownloadPreferences from "../components/pages/preferences/DownloadPreferences";
 
 function Preferences() {
-  return <div>Preferences</div>;
+  return (
+    <div className="realtime-grid">
+      <div className="row-one">
+        <PreferencesTitle />
+      </div>
+      <div className="row">{<DownloadPreferences />}</div>
+      <div className="row">{/* <SensorData /> */}</div>
+      {/* <PhenodeGrafana /> */}
+    </div>
+  );
 }
 
 export default Preferences;
