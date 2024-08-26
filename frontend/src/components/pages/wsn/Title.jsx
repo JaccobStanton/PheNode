b9c2532877b0e935b60a5dbb5d89bc79d6ab5f74
JaccobStanton <jstanton23@my.stlcc.edu>
2024-08-26 15:37:44 -0500

    Removed unneeded jsx code

--- frontend/src/components/pages/wsn/Title.jsx
+++ frontend/src/components/pages/wsn/Title.jsx
@@ -5,32 +5,12 @@ function WSNTitle() {
     <>
       <div className="grid-item page-title">Wireless Sensor Network</div>
       <div className="grid-item cell-wifi-last-measurements">
-        <div className="box">
-          <div className="last-measurement">
-            <span className="last-measurement-text">
+        <div className="imaging-measurements-box">
+          <div className="imaging-last-photo">
+            <span className="imaging-last-photo-text">
               Last measurements taken:
             </span>
-            <span className="measurement-date">October 31, 2022, 12:34pm</span>
-          </div>
-          <div className="bottom-box">
-            <div className="wifi-container">
-              <span className="wifi-text">WiFi: </span>
-              <div className="signal-blocks">
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-              </div>
-            </div>
-            <div className="cellular-container">
-              <span className="cellular-text">Cellular: </span>
-              <div className="signal-blocks">
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-                <div className="signal-block"></div>
-              </div>
-            </div>
+            <span className="last-photo-date">October 31, 2022, 12:34pm</span>
           </div>
         </div>
       </div>
