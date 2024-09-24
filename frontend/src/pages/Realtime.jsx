413169bb4e6308cb9d3ad67713d4d21ebc080d34
JaccobStanton <jstanton23@my.stlcc.edu>
2024-09-23 22:54:12 -0500

    Added responsivity based on viewports

--- frontend/src/pages/Realtime.jsx
+++ frontend/src/pages/Realtime.jsx
@@ -1,5 +1,5 @@
 // src/components/CustomGrid.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Realtime.css";
 import Title from "../components/pages/realtime/Title";
 import PheNode from "../components/pages/realtime/PheNode";
@@ -9,23 +9,53 @@ import RealtimeMobile from "../components/layouts/breakpoints/mobile/991px/Realt
 import RealtimeMobileSmall from "../components/layouts/breakpoints/mobile/767px/RealtimeMobileSmall";
 
 const Realtime = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  // Update the viewport width on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Conditional rendering logic based on viewport width
+  const renderResponsiveComponent = () => {
+    // Render PheNode and SensorData for 991px and above (including 1280px, 1440px, 1920px, 2560px)
+    if (viewportWidth >= 1280) {
+      return (
+        <>
+          <div className="row">
+            <PheNode />
+          </div>
+          <div className="row">
+            <SensorData />
+          </div>
+        </>
+      );
+    }
+    // Render RealtimeMobile for widths between 767px and 991px
+    else if (viewportWidth >= 990) {
+      return <RealtimeMobile />;
+    }
+    // Render RealtimeMobileSmall for widths below 767px
+    else {
+      return <RealtimeMobileSmall />;
+    }
+  };
+
   return (
     <div className="realtime-grid">
       <div className="row-one">
         <Title />
       </div>
-      <div className="row realtime-desktop">
-        <PheNode />
-      </div>
-      {/* <div className="row realtime-mobile">
-        <RealtimeMobile />
-      </div> */}
-      <div className="row realtime-mobile-767">
-        <RealtimeMobileSmall />
-      </div>
-      {/* <div className="row">
-        <SensorData />
-      </div> */}
+      <div>{renderResponsiveComponent()}</div>
       <PhenodeGrafana />
     </div>
   );
--- frontend/src/pages/WSN.jsx
+++ frontend/src/pages/WSN.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/WSN.css";
 import Sensor from "../components/pages/wsn/Sensor";
 import WSNTitle from "../components/pages/wsn/Title";
@@ -6,22 +6,49 @@ import SensorGrafana from "../components/pages/wsn/Grafana";
 import SensorMobile from "../components/layouts/breakpoints/mobile/991px/SensorMobile";
 
 function WSN() {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  // Update the viewport width on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const renderResponsiveMobileComponent = () => {
+    // Render Sensorfor 991px and above (including 1280px, 1440px, 1920px, 2560px)
+    if (viewportWidth >= 1280) {
+      return (
+        <>
+          <div className="row ">
+            <Sensor />
+          </div>
+        </>
+      );
+    }
+
+    // Render RealtimeMobileSmall for widths below 991px
+    else {
+      return (
+        <div className="row sensor-mobile">
+          <SensorMobile />
+        </div>
+      );
+    }
+  };
   return (
     <div className="realtime-grid">
       <div className="row-one">
         <WSNTitle />
       </div>
-
-      {/* Desktop Sensor */}
-      <div className="row sensor-desktop">
-        <Sensor />
-      </div>
-
-      {/* Mobile Sensor */}
-      <div className="row sensor-mobile">
-        <SensorMobile />
-      </div>
-
+      <div>{renderResponsiveMobileComponent()}</div>
       <SensorGrafana />
     </div>
   );
