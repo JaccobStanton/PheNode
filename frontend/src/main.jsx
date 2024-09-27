0518f585828ef479e80d6ea968ac5ce046fbe02e
JaccobStanton <jstanton23@my.stlcc.edu>
2024-09-27 16:36:11 -0500

    Wrapped app with SelectedDeviceContext.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { FleetDataProvider } from "./context/FleetDataContext.jsx";
+import { SelectedDeviceProvider } from "./context/SelectedDeviceContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <FleetDataProvider>
-        <App />
+        <SelectedDeviceProvider>
+          <App />
+        </SelectedDeviceProvider>
       </FleetDataProvider>
     </AuthProvider>
   </React.StrictMode>
