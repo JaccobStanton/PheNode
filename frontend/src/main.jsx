e9bcb9e5c5ed5e00b7363c790abb807ee7586289
JaccobStanton <jstanton23@my.stlcc.edu>
2024-10-02 15:49:25 -0500

    Removed strict mode and undeeded context that was wrapping application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { FleetDataProvider } from "./context/FleetDataContext.jsx";
-import { SelectedDeviceProvider } from "./context/SelectedDeviceContext.jsx";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./keycloak"; // Import the keycloak instance
+import AppContext from "./context/AppContext.jsx";
+
+console.log("Main file is rendering");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <FleetDataProvider>
-        <SelectedDeviceProvider>
-          <App />
-        </SelectedDeviceProvider>
-      </FleetDataProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <ReactKeycloakProvider authClient={keycloak}>
+    <AppContext>
+      <App />
+    </AppContext>
+  </ReactKeycloakProvider>
+  // </React.StrictMode>
 );
