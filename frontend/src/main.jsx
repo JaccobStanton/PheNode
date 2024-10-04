91ec74c01dae5062ff0807db469becdb60368cc1
JaccobStanton <jstanton23@my.stlcc.edu>
2024-10-04 11:39:10 -0500

    Wrapped application with new context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { ReactKeycloakProvider } from "@react-keycloak/web";
 import keycloak from "./keycloak"; // Import the keycloak instance
-import AppContext from "./context/AppContext.jsx";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
 console.log("Main file is rendering");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <ReactKeycloakProvider authClient={keycloak}>
-    <AppContext>
+    <AppContextProvider>
       <App />
-    </AppContext>
+    </AppContextProvider>
   </ReactKeycloakProvider>
   // </React.StrictMode>
 );
