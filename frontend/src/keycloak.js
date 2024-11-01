2dc01cc186653b5da16ab7350738f28bfb1088ad
JaccobStanton <jstanton23@my.stlcc.edu>
2024-10-31 20:36:10 -0500

    Using dev env, switched to .cloud

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 console.log("Creating Keycloak instance...");
 const keycloak = new Keycloak({
-  url: "https://auth.phenode.live",
+  url: "https://auth.phenode.cloud",
   realm: "grafana",
   clientId: "phenode",
 });
