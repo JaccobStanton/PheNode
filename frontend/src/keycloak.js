f003cf228c36c0b907067c6f4d23529cf25b2915
JaccobStanton <jstanton23@my.stlcc.edu>
2024-11-01 14:20:47 -0500

    Added code for token expiration

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -1,11 +1,24 @@
 import Keycloak from "keycloak-js";
+import {
+  KEYCLOAK_REALM,
+  KEYCLOAK_CLIENT_ID,
+  TOKEN_EXPIRATION,
+} from "./services/api";
 
 console.log("Creating Keycloak instance...");
 const keycloak = new Keycloak({
   url: "https://auth.phenode.cloud",
-  realm: "grafana",
-  clientId: "phenode",
+  realm: KEYCLOAK_REALM,
+  clientId: KEYCLOAK_CLIENT_ID,
 });
-
-console.log("Keycloak instance created:", keycloak);
+keycloak.onTokenExpired = () => {
+  keycloak
+    .updateToken(TOKEN_EXPIRATION)
+    .then(() => {
+      console.log("successfully get a new token");
+    })
+    .catch(() => {
+      console.log("failed to refresh token");
+    });
+};
 export default keycloak;
