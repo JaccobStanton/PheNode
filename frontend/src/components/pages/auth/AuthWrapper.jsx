059d4fd7aaf169ae05174b2d02e29d1f85652761
JaccobStanton <jstanton23@my.stlcc.edu>
2024-11-04 15:23:04 -0600

    Simplified AuthWrapper component.  Removed loading state

--- frontend/src/components/pages/auth/AuthWrapper.jsx
+++ frontend/src/components/pages/auth/AuthWrapper.jsx
@@ -1,70 +1,59 @@
-// AuthWrapper.jsx
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useKeycloak } from "@react-keycloak/web";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Loading from "./Loading";
 import useSessionTimeout from "../../../hooks/useSessionTimeout";
 
 const AuthWrapper = ({ children }) => {
   const { keycloak, initialized } = useKeycloak();
   const location = useLocation();
-  const [delayCompleted, setDelayCompleted] = useState(false);
-  const [loadingText, setLoadingText] = useState("Authenticating User");
+  const navigate = useNavigate();
 
-  const logoutInProgress = localStorage.getItem("logoutInProgress") === "true";
   const isAuthenticated = initialized && keycloak.authenticated;
 
   // Use the custom hook for session timeout
   useSessionTimeout(keycloak, isAuthenticated);
 
+  // Effect to reset state when the user logs out
   useEffect(() => {
-    let timer;
-
-    if (initialized && keycloak.authenticated) {
-      console.log("User is authenticated. Starting delay timer...");
-
-      // Remove 'logoutInProgress' flag if it exists
-      localStorage.removeItem("logoutInProgress");
-
-      // Set loading text for authenticated state
-      setLoadingText("Setting up your environment");
-
-      // Start the 11-second timer when the user is authenticated
-      timer = setTimeout(() => {
-        console.log("Delay completed.");
-        setDelayCompleted(true);
-      }, 11000);
+    if (
+      initialized &&
+      !keycloak.authenticated &&
+      location.pathname !== "/logged-out"
+    ) {
+      console.log("User is not authenticated.");
+      localStorage.removeItem("delayCompleted");
     }
+  }, [initialized, keycloak.authenticated, location.pathname]);
 
-    // Cleanup function to clear the timer if the component unmounts
-    return () => {
-      if (timer) {
-        clearTimeout(timer);
-      }
-    };
-  }, [initialized, keycloak.authenticated]);
+  // Effect to handle redirection if user is authenticated and on '/logged-out'
+  useEffect(() => {
+    if (
+      initialized &&
+      keycloak.authenticated &&
+      location.pathname === "/logged-out"
+    ) {
+      console.log("Redirecting authenticated user away from /logged-out");
+      navigate("/");
+    }
+  }, [initialized, keycloak.authenticated, location.pathname, navigate]);
 
   // Show Loading component while Keycloak is initializing
   if (!initialized) {
-    if (logoutInProgress || location.pathname === "/logout") {
-      console.log(
-        "Keycloak is not initialized, but logout is in progress. Showing logout screen..."
-      );
-      return <Loading loadingText="Logging out" />;
-    } else {
-      console.log("Keycloak is not initialized yet, showing loading page...");
-      return <Loading loadingText="Authenticating User" />;
-    }
+    console.log("Keycloak is not initialized yet, showing loading page...");
+    return <Loading loadingText="Loading" />;
   }
 
   // If Keycloak is initialized but the user is not authenticated
   if (!keycloak.authenticated) {
-    if (logoutInProgress || location.pathname === "/logout") {
+    if (
+      location.pathname === "/logout" ||
+      location.pathname === "/logged-out"
+    ) {
       console.log(
-        "User is not authenticated and logout is in progress. Showing logout screen..."
+        "User is not authenticated and on logout path. Showing logout screen..."
       );
-      // Display Loading component with "Logging out" message
-      return <Loading loadingText="Logging out" />;
+      return children;
     } else {
       console.log("User is not authenticated. Redirecting to login...");
       keycloak.login({ redirectUri: window.location.origin + "/" });
@@ -72,14 +61,7 @@ const AuthWrapper = ({ children }) => {
     }
   }
 
-  // User is authenticated but delay is not completed
-  if (!delayCompleted) {
-    console.log("Delay not completed yet. Showing loading page...");
-    return <Loading loadingText={loadingText} />;
-  }
-
-  // User is authenticated and delay is completed
-
+  // User is authenticated, show the children components
   return <>{children}</>;
 };
 
