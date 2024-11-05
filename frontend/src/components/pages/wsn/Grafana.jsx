2cb80d2fbec5bd82576c10ff203e1750ff892714
JaccobStanton <jstanton23@my.stlcc.edu>
2024-11-04 21:48:15 -0600

    Moved file to different directory

--- frontend/src/components/pages/wsn/Grafana.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../../../styles/WSN.css";
-
-function SensorGrafana() {
-  return <div className="sensor-grafana-box">sensor-graphana</div>;
-}
-
-export default SensorGrafana;
