407667df2a5fa5f38a3ab4b2da7839e96d456ead
JaccobStanton <jstanton23@my.stlcc.edu>
2024-11-15 11:47:40 -0600

    Wrapped preferences component in protected route logic so someone who is not an admin, cannot ask

--- frontend/src/components/pages/download/Title.jsx
+++ frontend/src/components/pages/download/Title.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useKeycloak } from "@react-keycloak/web";
+import { toast } from "react-toastify";
+import { userHasRole } from "../../../utils/authUtils";
 import DataPreferenceInactive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Inactive.svg";
 import DataPreferenceActive from "../../../assets/toggle_buttons/Download_Data_Preferences_Icon_Active.svg";
 
 function DownloadTitle() {
   const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
+  const { keycloak } = useKeycloak();
+  const hasEditorRole = userHasRole(keycloak, "phenode-editor");
 
   const handleNavigate = () => {
-    navigate("/preferences");
+    if (hasEditorRole) {
+      navigate("/preferences");
+    } else {
+      toast.error(
+        "Permissions required to access this function. Please contact your admin."
+      );
+    }
   };
+
   return (
     <>
       <div className="grid-item page-title">Download Data</div>
